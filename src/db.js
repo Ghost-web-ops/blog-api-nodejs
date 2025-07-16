315a7b640ffb0c5212738aae3da29ce5e80cff94
Ghost-web-ops <omaryasser011549@gmail.com>
2025-07-16 15:29:45 +0300

    update

--- src/db.js
+++ src/db.js
@@ -1,11 +1,12 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_DATABASE,
+  // password: process.env.DB_PASSWORD,
+  // port: process.env.DB_PORT,
+  DATABASE_URL: process.env.DATABASE_URL,
 });
 
 
